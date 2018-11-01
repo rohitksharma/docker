eebc126b57d69216af19090f740719a23161a2c6
Rohit Sharma <rsharma2602@gmail.com>
2018-10-31 22:51:02 -0700

    syntax fix
    syntax fix

--- tests/test_es_plugins.py
+++ tests/test_es_plugins.py
@@ -5,6 +5,7 @@ from requests import codes
 
 def test_IngestUserAgentPlugin_is_installed(elasticsearch):
     # Ensure IngestUserAgentPlugin is present on all nodes
+    #
     for nodeplugins in elasticsearch.get_node_plugins():
         plugin_classnames = [plugin['classname'] for plugin in nodeplugins]
         assert 'org.elasticsearch.ingest.useragent.IngestUserAgentPlugin' in plugin_classnames
